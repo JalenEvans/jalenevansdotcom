02cd0a40a8d3a67e1f009358c7d8239b3bfe2df7
JalenEvans <jalenemmanuelevans@gmail.com>
2026-01-27 19:27:30 -0500

    Setup Prettier

--- my-website/src/content/config.ts
+++ my-website/src/content/config.ts
@@ -1,27 +1,27 @@
-import { defineCollection, z } from "astro:content";
+import { defineCollection, z } from 'astro:content'
 
 const blog = defineCollection({
-  type: "content",
+  type: 'content',
   schema: z.object({
     title: z.string(),
     pubDate: z.coerce.date(), // ISO String: YYYY-MM-DDThh:mm:ss
-    author: z.string().default("Jalen Evans"),
+    author: z.string().default('Jalen Evans'),
     description: z.string(),
     tags: z.array(z.string()),
     image: z.string().optional(),
   }),
-});
+})
 
 const project = defineCollection({
-  type: "content",
+  type: 'content',
   schema: z.object({
     title: z.string(),
     startDate: z.coerce.date(), // ISO String: YYYY-MM-DDThh:mm:ss
-    contributors: z.array(z.string()).default(["Jalen Evans"]),
+    contributors: z.array(z.string()).default(['Jalen Evans']),
     description: z.string(),
     tags: z.array(z.string()),
     image: z.string().optional(),
   }),
-});
+})
 
-export const collections = { blog, project };
+export const collections = { blog, project }
