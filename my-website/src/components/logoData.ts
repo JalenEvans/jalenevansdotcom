b22d1e70947b98ca11c63e3d270e63883eb2c53a
JalenEvans <jalenemmanuelevans@gmail.com>
2026-01-30 16:22:11 -0500

    Added a gradient

--- my-website/src/components/logoData.ts
+++ my-website/src/components/logoData.ts
@@ -1,4 +1,7 @@
 import AstroLogo from '../assets/Logos/astro.svg?raw'
+import FlutterLogo from '../assets/Logos/flutter.svg?raw'
+import FirebaseLogo from '../assets/Logos/firebase.svg?raw'
+import JavaLogo from '../assets/Logos/java.svg?raw'
 
 export type LogoData = {
   title: string
@@ -12,4 +15,22 @@ const ASTRO_DATA: LogoData = {
   hoverColor: '#BC52EE',
 }
 
-export { ASTRO_DATA }
+const FLUTTER_DATA: LogoData = {
+  title: 'Flutter',
+  rawPath: FlutterLogo,
+  hoverColor: '#02569B',
+}
+
+const FIREBASE_DATA: LogoData = {
+  title: 'Firebase',
+  rawPath: FirebaseLogo,
+  hoverColor: '#DD2C00',
+}
+
+const JAVA_DATA: LogoData = {
+  title: 'Java',
+  rawPath: JavaLogo,
+  hoverColor: '#EA2D2E',
+}
+
+export { ASTRO_DATA, FLUTTER_DATA, FIREBASE_DATA, JAVA_DATA }
