4db08038ca198f99d1f7699013073e271a1ebc5c
JalenEvans <jalenemmanuelevans@gmail.com>
2026-02-01 09:19:10 -0500

    Added headshot circle and NavBar path teller

--- my-website/src/components/logoData.ts
+++ my-website/src/components/logoData.ts
@@ -1,7 +1,10 @@
 import AstroLogo from '../assets/Logos/astro.svg?raw'
+import FastAPI from '../assets/Logos/fastapi.svg?raw'
 import FlutterLogo from '../assets/Logos/flutter.svg?raw'
 import FirebaseLogo from '../assets/Logos/firebase.svg?raw'
 import JavaLogo from '../assets/Logos/java.svg?raw'
+import PostgreSQLLogo from '../assets/Logos/postgresql.svg?raw'
+import ReactLogo from '../assets/Logos/react.svg?raw'
 
 export type LogoData = {
   title: string
@@ -13,6 +16,12 @@ const ASTRO_DATA: LogoData = {
   title: 'Astro',
   rawPath: AstroLogo,
   hoverColor: '#BC52EE',
+}
+
+const FASTAPI_DATA: LogoData = {
+  title: 'FastAPI',
+  rawPath: FastAPI,
+  hoverColor: '#009688',
 }
 
 const FLUTTER_DATA: LogoData = {
@@ -33,4 +42,24 @@ const JAVA_DATA: LogoData = {
   hoverColor: '#EA2D2E',
 }
 
-export { ASTRO_DATA, FLUTTER_DATA, FIREBASE_DATA, JAVA_DATA }
+const POSTGRESQL_DATA: LogoData = {
+  title: 'PostgreSQL',
+  rawPath: PostgreSQLLogo,
+  hoverColor: '#4169E1',
+}
+
+const REACT_DATA: LogoData = {
+  title: 'React',
+  rawPath: ReactLogo,
+  hoverColor: '#61DAFB',
+}
+
+export {
+  ASTRO_DATA,
+  FASTAPI_DATA,
+  FLUTTER_DATA,
+  FIREBASE_DATA,
+  JAVA_DATA,
+  POSTGRESQL_DATA,
+  REACT_DATA,
+}
