6eecdf16b15c3b1611b004ba1cc4b4890fe2315b
JalenEvans <jalenemmanuelevans@gmail.com>
2026-02-04 11:48:37 -0500

    Refactored ProjectCard to be mapped

--- my-website/src/content/config.ts
+++ my-website/src/content/config.ts
@@ -5,23 +5,25 @@ const blog = defineCollection({
   schema: z.object({
     id: z.string(),
     title: z.string(),
-    pubDate: z.coerce.date(), // ISO String: YYYY-MM-DDThh:mm:ss
     author: z.string().default('Jalen Evans'),
+    pubDate: z.coerce.date(), // ISO String: YYYY-MM-DDThh:mm:ss
     description: z.string(),
-    tags: z.array(z.string()),
     mainImage: z.string().optional(),
+    tags: z.array(z.string()),
   }),
 })
 
 const project = defineCollection({
   type: 'content',
   schema: z.object({
+    id: z.string(),
     title: z.string(),
     startDate: z.coerce.date(), // ISO String: YYYY-MM-DDThh:mm:ss
     contributors: z.array(z.string()).default(['Jalen Evans']),
     description: z.string(),
+    tools: z.array(z.string()),
+    mainImage: z.string().optional(),
     tags: z.array(z.string()),
-    image: z.string().optional(),
   }),
 })
 
